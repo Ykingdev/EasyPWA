c471ba1673d16cd4ca6368e64614aba9b6525607
Y-Shinden King <yshinden.king@student.rocva.nl>
2022-11-26 00:51:44 +0100

    Very small projects no minor commits! this is everything you need

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+if ('serviceWorker' in navigator) { navigator.serviceWorker.register('serviceworker.js') };
+
+// code below will cache everything! this means changes made to icons or html wont be visible untill you turn this off!
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+      caches
+        .open("v1")
+        .then((cache) =>
+          cache.addAll([
+            "/"
+          ])
+        )
+    );
+  });
+  
+  self.addEventListener("fetch", (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        // caches.match() always resolves
+        // but in case of success response will have value
+        if (response !== undefined) {
+          return response;
+        } else {
+          return fetch(event.request)
+            .then((response) => {
+              // response may be used only once
+              // we need to save clone to put one copy in cache
+              // and serve second one
+              let responseClone = response.clone();
+  
+              caches.open("v1").then((cache) => {
+                cache.put(event.request, responseClone);
+              });
+              return response;
+            })
+            .catch(() => caches.match("/"));
+        }
+      })
+    );
+  });
